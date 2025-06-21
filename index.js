9ed1c04a0f06bce3812abdbe931075da90882393
shalini <s.rathore43@gmail.com>
2025-06-21 10:19:44 +0530

    fsd

--- index.js
+++ index.js
@@ -5,27 +5,28 @@ const server = createServer(app);
 const {join} = require("path")
 
 const { Server } = require('socket.io');
-
 const io = new Server(server);
-
 app.get("/",(req,res)=>
 {
     res.sendFile(join(__dirname,"index.html"))
 })
+
 io.on('connection', (socket) => {
-  console.log('a user connected id: '+ socket.id);
+ console.log('a user connected id: '+ socket.id);
   let clientname = ""
+  let clientid=""
   socket.on('ClientName',(username)=>
   {
-      console.log(username)
+      // console.log(username)     
       clientname = username
+      clientid= socket.id//Math.round(Math.random()*1252);  
+      console.log(`username ${username} and client id ${clientid}`)
   })
-  socket.on("ClientMsg",(msg)=>
+  socket.on("ClientMsg",(msg,id)=>
   {
-    msg = (clientname +" : "+msg)
-    io.emit("serverMsg",(msg))
+    msg = (clientname +"---- "+clientid+" : "+msg)
+    io.to(id).emit("serverMsg",(msg))
   })
-  
 
    socket.on('disconnect', () => {
     console.log('user disconnected');
